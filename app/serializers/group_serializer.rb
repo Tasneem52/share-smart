65e34fe7ef88129197038e8fc6e49f0cd3fe8ec1
Tasneem Amreliwala <tasneemec7@gmail.com>
2019-05-20 02:10:50 -0400

    Added group members

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,3 +1,3 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :status, :icon, :created_at, :memberships, :invitations, :products
+  attributes :id, :name, :description, :status, :icon, :created_at, :memberships, :invitations, :products, :users
 end
