6e31c4c70201dd22738c02c7c8d51b43d808d73a
Tasneem Amreliwala <tasneemec7@gmail.com>
2019-06-09 20:43:08 -0400

    user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe User do
+  it { should have_valid(:name).when("Tin") }
+  it { should_not have_valid(:name).when('', nil) }
+  it { should have_valid(:location).when("Boston")}
+  it { should_not have_valid(:location).when("", nil) }
+end
